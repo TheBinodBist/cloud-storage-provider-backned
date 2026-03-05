cdd553fee76eeb0af1527094f6035b6cc9dfb3db
Binod Bist <thebinodbist@gmail.com>
2026-03-05 20:06:09 +0545

    create-post api

--- src/app.js
+++ src/app.js
@@ -3,8 +3,10 @@ const app = express();
 const multer = require('multer');
 app.use(express.json());
 const storage = multer({storage: multer.memoryStorage()});
-app.post('/create-post',(req,res)=>{
+const upload = multer({storage: multer.memoryStorage()});
+app.post('/create-post',upload.single('image'),(req,res)=>{
     console.log(req.body);
+    console.log(req.file);
     res.send("Post created successfully");
 })
 
