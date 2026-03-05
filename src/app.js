f73626cda7c1c34d89a689c445c7569e60a51b7c
Binod Bist <thebinodbist@gmail.com>
2026-03-05 20:04:13 +0545

    create-post api created

--- src/app.js
+++ src/app.js
@@ -1,4 +1,11 @@
 const  express = require('express');
 const app = express();
+const multer = require('multer');
+app.use(express.json());
+const storage = multer({storage: multer.memoryStorage()});
+app.post('/create-post',(req,res)=>{
+    console.log(req.body);
+    res.send("Post created successfully");
+})
 
 module.exports = app;
