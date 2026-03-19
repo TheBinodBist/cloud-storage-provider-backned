9a079966753c148228cc71e3a7b95b1aba480175
Binod Bist <thebinodbist@gmail.com>
2026-03-19 17:07:14 +0545

    db starage.service updated

--- src/db/db.js
+++ src/db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const connectDB= async()=>{
-    await mongoose.connect("mongodb://localhost:27017/CSP")
+    await mongoose.connect("mongodb+srv://dev:dev@cluster0.2jgua5n.mongodb.net/cloud-storage-provider")
     .then(()=>(console.log("Connected to MongoDB")))
     .catch((err)=>(console.log("Error connecting to MongoDB:",err)));   
 }
--- src/service/storage.services.js
+++ src/service/storage.services.js
@@ -1,5 +1,17 @@
 const ImageKit = require('@imagekit/nodejs')
 
 const imagekit = new ImageKit({
-    
+    privateKey:"private_0vDZIYVGez8WCgOlGA/SrQx9CjM="
 })
+
+
+async function uploadFile(buffer){
+    const result = await  imagekit.files.upload({
+        file:buffer,
+        fileName :"image.jpg",
+
+    })
+    return result;
+}
+
+module.exports = uploadFile;
